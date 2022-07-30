4856596291bc453df4ce0ce19dbddfef7d08a235
Uzair Afridi <uzairafridi00@gmail.com>
2022-07-30 12:25:06 +0500

    Update Home.jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -12,7 +12,7 @@ function Home() {
 
   useEffect(() => {
     fetch(
-      "https://api.themoviedb.org/3/movie/popular?api_key=4e44d9029b1270a757cddc766a1bcb63&language=en-US"
+      "YOUR API KEY"
     )
       .then((res) => res.json())
       .then((data) => setPopularMovies(data.results));
