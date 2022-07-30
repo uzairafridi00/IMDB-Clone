d7cdbe9004bb7cca2f7a1342f27436caff76cef1
Uzair Afridi <uzairafridi00@gmail.com>
2022-07-30 12:23:18 +0500

    Home Carousal Completed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import Header from './components/header/Header';
+import Home from './pages/home/Home';
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
        <Router>
         <Header />
         <Routes>
-          <Route index element={<h1>Index</h1>}></Route>
+          <Route index element={<Home />}></Route>
           <Route path='movie/:id' element={<h1>Movie Detail</h1>}></Route>
           <Route path='movies/:type' element={<h1>Movies List pAge</h1>}></Route>
           <Route path='/*' element={<h1>Error Page</h1>}></Route>
